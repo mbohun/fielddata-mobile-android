7eddb2de1f9e71249b59d57d82d3261a80b8988b
chrisala <chris.godwin.ala@gmail.com>
2013-05-29 23:51:34 +0000

    Getting the gradle build to work.
    git-svn-id: http://ala-mobile.googlecode.com/svn/trunk/fielddata-mobile@475 6a6df900-09af-505f-f2bd-838eb8a94856

--- libraries/setupLibraries.bash
+++ libraries/setupLibraries.bash
@@ -1,11 +1,26 @@
 #!/bin/bash
+cd ${0%/*}
+pwd
+if [ ! -d "ActionBarSherlock" ]; then
+    echo "Cloning ActionBarSherlock"
+    git clone -b dev https://github.com/JakeWharton/ActionBarSherlock.git
+fi
+if [ ! -d "Android-ViewPagerIndicator" ]; then
+    echo "Cloning Android-ViewPagerIndicator"
+    git clone https://github.com/JakeWharton/Android-ViewPagerIndicator.git
+    cp android-viewpagerindicator.gradle Android-ViewPagerIndicator/library/build.gradle
+fi
+if [ ! -d "android-mapviewballoons" ]; then
+    echo "Cloning android-mapviewballoons"
+    git clone https://github.com/jgilfelt/android-mapviewballoons.git
+    cp android-mapviewballoons.gradle android-mapviewballoons/android-mapviewballoons/build.gradle
 
-git clone -b dev https://github.com/JakeWharton/ActionBarSherlock.git
-git clone https://github.com/JakeWharton/Android-ViewPagerIndicator.git
-git clone https://github.com/jgilfelt/android-mapviewballoons.git
-cp -R $ANDROID_HOME/extras/google/google_play_services/libproject/google-play-services_lib .
-cp android-mapviewballoons.gradle android-mapviewballoons/android-mapviewballoons/build.gradle
-cp google-play-services_lib.gradle google-play-services_lib/build.gradle
-cp android-viewpagerindicator.gradle Android-ViewPagerIndicator/library/build.gradle
+fi
+if [ ! -d "google-play-services_lib" ]; then
+    echo "Copying google-play-services_lib"
+    cp -R $ANDROID_HOME/extras/google/google_play_services/libproject/google-play-services_lib .
+    cp google-play-services_lib.gradle google-play-services_lib/build.gradle
+fi
+
 
 
