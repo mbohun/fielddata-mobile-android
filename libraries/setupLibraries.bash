a6845a5ba105428939f4d68bde159d15067b0c9b
chrisala <chris.godwin.ala@gmail.com>
2013-05-29 07:12:07 +0000

    Adding build files.
    git-svn-id: http://ala-mobile.googlecode.com/svn/trunk/fielddata-mobile@461 6a6df900-09af-505f-f2bd-838eb8a94856

---
+++ libraries/setupLibraries.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone -b dev https://github.com/JakeWharton/ActionBarSherlock.git
+git clone https://github.com/JakeWharton/Android-ViewPagerIndicator.git
+git clone https://github.com/jgilfelt/android-mapviewballoons.git
+cp -R $ANDROID_HOME/extras/google/google_play_services/libproject/google-play-services_lib .
+cp androidmapviewballoons.gradle android-mapviewballoons/android-mapviewballoons/build.gradle
+cp google-play-services_lib.gradle google-play-services_lib/build.gradle
+cp android-viewpagerindicator.gradle Android-ViewPagerIndicator/library/build.gradle
+
+
