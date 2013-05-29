aab64259a9ea2d2656a0a37799c94ba1b0452db1
chrisala <chris.godwin.ala@gmail.com>
2013-05-29 07:22:55 +0000

    fixed typo
    git-svn-id: http://ala-mobile.googlecode.com/svn/trunk/fielddata-mobile@465 6a6df900-09af-505f-f2bd-838eb8a94856

--- libraries/setupLibraries.bash
+++ libraries/setupLibraries.bash
@@ -4,7 +4,7 @@ git clone -b dev https://github.com/JakeWharton/ActionBarSherlock.git
 git clone https://github.com/JakeWharton/Android-ViewPagerIndicator.git
 git clone https://github.com/jgilfelt/android-mapviewballoons.git
 cp -R $ANDROID_HOME/extras/google/google_play_services/libproject/google-play-services_lib .
-cp androidmapviewballoons.gradle android-mapviewballoons/android-mapviewballoons/build.gradle
+cp android-mapviewballoons.gradle android-mapviewballoons/android-mapviewballoons/build.gradle
 cp google-play-services_lib.gradle google-play-services_lib/build.gradle
 cp android-viewpagerindicator.gradle Android-ViewPagerIndicator/library/build.gradle
 
